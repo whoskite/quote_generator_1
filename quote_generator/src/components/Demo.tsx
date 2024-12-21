b2da1c33dff889c36848c1608305791f46206c67
KITE <whoskitemedia@gmail.com>
2024-12-20 16:52:29 -0800

    test

--- quote_generator/src/components/Demo.tsx
+++
@@ -1,7 +0,0 @@
-export default function Demo() {
-    return (
-      <div className="w-[300px] mx-auto py-4 px-2">
-        <h1 className="text-2xl font-bold text-center mb-4">Quote Generator Test</h1>
-      </div>
-    );
-  }
